d2919c6f3add0a801e847fb3800c18148c823126
Tom C <tomc@3gmagic.com>
2020-01-16 09:54:45 +0000

    add gov

--- public/javascripts/iframes.js
+++ public/javascripts/iframes.js
@@ -3,21 +3,34 @@
 var ifrm = document.createElement('iframe');
 ifrm.setAttribute('id', 'parlement');
 document.body.appendChild(ifrm); // to place at end of document
-parlement.setAttribute('src', 'parlement.html');
+parlement.setAttribute('src', 'gov');
 
 for (var i = 0; i<3; i++) {
-    var ifrm1 = document.createElement('iframe');
+    var ifrm = document.createElement('iframe');
     ifrm.setAttribute('id', 'constituency'+i); // assign an id
     // to place before another page element
-    var el = document.getElementById('parlement');
-    el.parentNode.insertBefor(ifrm1, el);
+   // var el = document.getElementById('parlement');
+    //el.parentNode.insertBefor(ifrm1, el);
+    document.body.appendChild(ifrm)
     ifrm.setAttribute('src', 'constituency.html');
+
+    for (var j=0; j<3; j++){
+        var ifrm = document.createElement('iframe');
+        ifrm.setAttribute('id', 'voter'+j+i); // assign an id
+        // to place before another page element
+        var el = document.getElementById('constituency'+i);
+        //el.parentNode.insertAfter(ifrm, el);
+        document.body.appendChild(ifrm)
+        ifrm.setAttribute('src', 'candidate.html');
+    }
 
     for (var j=0; j<10; j++){
         var ifrm = document.createElement('iframe');
         ifrm.setAttribute('id', 'voter'+j+i); // assign an id
         // to place before another page element
         var el = document.getElementById('constituency'+i);
-        el.parentNode.insertAfter(ifrm, el);
+        //el.parentNode.insertAfter(ifrm, el);
+        document.body.appendChild(ifrm)
+        ifrm.setAttribute('src', 'voter.html');
     }
 } 
